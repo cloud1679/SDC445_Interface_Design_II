87d9d667709fa812d0f3a62691686422b5e13e87
cloud1679 <cloud1679@icloud.com>
2026-07-25 00:13:48 -0400

    project remove
    All projects were in the folder of this horse, project manager accidentally, and now have been removed

--- Horse_Manager_ricbur1110-project-app/PA_statehooks/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders todo list heading', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/todo list/i);
-  expect(headingElement).toBeInTheDocument();
-});
